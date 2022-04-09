0a62103695e7b838b5554c82287f01acdbf08381
Juanbla <juananvilla96@gmail.com>
2022-04-09 15:19:54 +0200

    Actu

---
+++ src/components/Car.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>Coche</h2>
+    <p>Marca: {{ brand }}</p>
+</template>
+
+<script>
+export default {
+
+    setup(){
+        const brand =  "Audi";
+        const model =  "A4";
+        const colors =  ["Azul","Morado", "Marron"];
+        const price = 48000;
+        const powers = [60,80,120,150,200,220,300,385,480,625];
+        const mensaje = {
+            title: "Estoy bien",
+            text: "Welcome",
+        };
+
+        return {
+            brand,
+            model,
+            colors,
+            price,
+            power,
+            mensaje,
+        };
+    },
+
+};
+</script>
+
+<style>
+
+</style>
